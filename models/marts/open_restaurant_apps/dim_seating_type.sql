b91ca77269636e489a97af271108ecc5873282ff
Leticia Huang <leticia.huang@baruchmail.cuny.edu>
2026-03-31 01:03:34 +0000

    updated tables

--- models/marts/open_restaurant_apps/dim_seating_type.sql
+++ models/marts/open_restaurant_apps/dim_seating_type.sql
@@ -1,5 +1,5 @@
 -- Seating type dimension for open restaurant seating applications
-{{ config(materialized='table', schema='nyc_311_dot') }}
+{{ config(materialized='table', schema='nyc_open_restaurant_apps') }}
 WITH seating_types AS (
    SELECT DISTINCT 
     seating_interest_sidewalk AS seating_interest,
