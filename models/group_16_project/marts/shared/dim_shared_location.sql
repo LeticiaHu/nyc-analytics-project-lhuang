066cb509cfc8311f0e272f5f05ef19bc6aa5865c
Leticia Huang <leticia.huang@baruchmail.cuny.edu>
2026-04-29 13:21:08 +0000

    Updates done to dim_shared_location to add missing columns

--- models/group_16_project/marts/shared/dim_shared_location.sql
+++ models/group_16_project/marts/shared/dim_shared_location.sql
@@ -2,16 +2,20 @@ WITH locations AS (
 
     SELECT DISTINCT
         borough,
-        incident_zip AS zip_code
+        incident_zip AS zip_code,
+        street_name
     FROM {{ ref('stg_nyc_311_vehicle_complaints') }}
     WHERE borough IS NOT NULL
        OR incident_zip IS NOT NULL 
+       AND  street_name IS NOT NULL
        
     UNION DISTINCT
 
     SELECT DISTINCT 
         borough,
-        zip_code
+        zip_code,
+        cross_street_name,
+        off_street_name
     FROM {{ ref('stg_nyc_vehicle_crashes') }}
     WHERE borough IS NOT NULL
        OR zip_code IS NOT NULL 
