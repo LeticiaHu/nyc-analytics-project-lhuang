b35f14d131bfaea53652194f0d2d98603db828b9
Leticia Huang <leticia.huang@baruchmail.cuny.edu>
2026-04-29 23:25:48 +0000

    Updated dim_shared_date to include date and time

--- models/group_16_project/marts/shared/dim_shared_date.sql
+++ models/group_16_project/marts/shared/dim_shared_date.sql
@@ -1,40 +1,43 @@
+WITH dates AS (
+   -- Get dates (dates, no time included) from 311 requests
+   SELECT DISTINCT 
+    CAST(created_date AS DATE) AS full_date,
+    FORMAT_TIME('$H:$M', CAST(created_date AS TIME)) AS hour_of_day
+   FROM {{ ref('stg_nyc_311_vehicle_complaints') }}
+   WHERE created_date IS NOT NULL
 
-WITH datetimes AS (
+   UNION DISTINCT
 
-    -- Get created datetimes from 311 requests
-    SELECT DISTINCT
-        created_date AS datetime_value
-    FROM {{ ref('stg_nyc_311_vehicle_complaints') }}
-    WHERE created_date IS NOT NULL
-
-    UNION DISTINCT
-
-    -- Get crash datetimes from vehicle crashes
-    SELECT DISTINCT
-        crash_datetime AS datetime_value
-    FROM {{ ref('stg_nyc_vehicle_crashes') }}
-    WHERE crash_datetime IS NOT NULL
+   -- Get dates from vehicle crashes
+   SELECT DISTINCT 
+    CAST(crash_date AS DATE) AS full_date,
+    FORMAT_TIME('%H',PARSE_TIME('%H:%M', crash_time)) AS hour_of_day,
+   FROM {{ ref('stg_nyc_vehicle_crashes') }}
+   WHERE crash_date IS NOT NULL
 ),
 
-date_dimension AS (
+final AS (
 
     SELECT
-        {{ dbt_utils.generate_surrogate_key(['datetime_value']) }} AS date_key,
+        {{ dbt_utils.generate_surrogate_key(['full_date']) }} AS date_key,
 
-        CAST(datetime_value AS DATE) AS full_date,
+        full_date,
+        EXTRACT(YEAR FROM full_date) AS year,
+        EXTRACT(MONTH FROM full_date) AS month,
+        EXTRACT(DAY FROM full_date) AS day,
+        FORMAT_DATE('%A', full_date) AS day_of_week,
+        FORMAT_DATE('%B', full_date) AS month_name,
+        EXTRACT(DAYOFWEEK FROM full_date) IN (1, 7) AS is_weekend,
+        hour_of_day,
+          
+     CASE
+       WHEN hour_of_day < "12:00" THEN "AM"
+       ELSE "PM"
+    END AS time_of_day
+    
 
-        FORMAT_DATE('%A', CAST(datetime_value AS DATE)) AS day,
-        FORMAT_DATE('%B', CAST(datetime_value AS DATE)) AS month,
-        EXTRACT(YEAR FROM CAST(datetime_value AS DATE)) AS year,
+    FROM dates
 
-        CAST(datetime_value AS TIMESTAMP) AS time,
-
-        EXTRACT(HOUR FROM CAST(datetime_value AS TIMESTAMP)) < 12 AS is_am,
-        EXTRACT(HOUR FROM CAST(datetime_value AS TIMESTAMP)) >= 12 AS is_pm,
-
-        EXTRACT(DAYOFWEEK FROM CAST(datetime_value AS DATE)) IN (1, 7) AS is_weekend
-
-    FROM datetimes
 )
 
-SELECT * FROM date_dimension
+SELECT * FROM final
