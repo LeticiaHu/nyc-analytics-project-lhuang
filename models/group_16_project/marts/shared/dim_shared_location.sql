9cdce64024e2cc55f5a21282609e8d41ea79c21f
Leticia Huang <leticia.huang@baruchmail.cuny.edu>
2026-04-29 13:33:32 +0000

    Final update made to dim_shared_location

--- models/group_16_project/marts/shared/dim_shared_location.sql
+++ models/group_16_project/marts/shared/dim_shared_location.sql
@@ -3,32 +3,43 @@ WITH locations AS (
     SELECT DISTINCT
         borough,
         incident_zip AS zip_code,
-        street_name
+        street_name,
+        CAST(NULL AS STRING) AS cross_street_name,
+        CAST(NULL AS STRING) AS off_street_name
     FROM {{ ref('stg_nyc_311_vehicle_complaints') }}
     WHERE borough IS NOT NULL
        OR incident_zip IS NOT NULL 
-       AND  street_name IS NOT NULL
+       OR street_name IS NOT NULL
        
     UNION DISTINCT
 
     SELECT DISTINCT 
         borough,
         zip_code,
+        CAST(NULL AS STRING) AS street_name,
         cross_street_name,
         off_street_name
     FROM {{ ref('stg_nyc_vehicle_crashes') }}
     WHERE borough IS NOT NULL
-       OR zip_code IS NOT NULL 
+       OR zip_code IS NOT NULL
 ),
 
 final AS (
 
     SELECT
-        {{ dbt_utils.generate_surrogate_key(['borough', 'zip_code']) }} AS location_key,
+        {{ dbt_utils.generate_surrogate_key([
+            'borough',
+            'zip_code',
+            'street_name',
+            'cross_street_name',
+            'off_street_name'
+        ]) }} AS location_key,
         borough,
-        zip_code
+        zip_code,
+        street_name,
+        cross_street_name,
+        off_street_name
     FROM locations
 )
 
 SELECT * FROM final
-
