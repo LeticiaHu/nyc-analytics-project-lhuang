527e1d25b3572226d203eea4a4e9ea9ea5d2be96
Leticia Huang <leticia.huang@baruchmail.cuny.edu>
2026-05-05 02:07:55 +0000

    Updating to fix location

--- models/group_16_project/marts/shared/dim_shared_location.sql
+++ models/group_16_project/marts/shared/dim_shared_location.sql
@@ -7,9 +7,6 @@ WITH locations AS (
         UPPER(TRIM(CAST(cross_street_1 AS STRING))) AS cross_street_name,
         CAST(NULL AS STRING) AS off_street_name
     FROM {{ ref('stg_nyc_311_vehicle_complaints') }}
-    WHERE borough IS NOT NULL
-      AND incident_zip IS NOT NULL
-      AND street_name IS NOT NULL
 
     UNION DISTINCT
 
@@ -20,12 +17,10 @@ WITH locations AS (
         UPPER(TRIM(CAST(cross_street_name AS STRING))) AS cross_street_name,
         UPPER(TRIM(CAST(off_street_name AS STRING))) AS off_street_name
     FROM {{ ref('stg_nyc_vehicle_crashes') }}
-    WHERE borough IS NOT NULL
-      AND zip_code IS NOT NULL
-      AND on_street_name IS NOT NULL
 ),
 
 final AS (
+
     SELECT
         {{ dbt_utils.generate_surrogate_key([
             'borough',
